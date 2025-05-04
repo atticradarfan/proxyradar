b44adaad2badaa232c64f2d65c8b3c84d35f744f
atticradarfan <henryabrindle@gmail.com>
2025-05-04 19:22:11 -0400

    Update proxy-server.js

--- proxy-server.js
+++ proxy-server.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors'); // <--- NEW
+const cors = require('cors'); // To allow cross-origin requests
 
 const app = express();
 
-app.use(cors()); // <--- NEW
+app.use(cors()); // Enable CORS for all requests
 
-// Proxy route
+// Proxy route for radar images
 app.get('/proxy/radar/:site/:product/:timestamp.png', async (req, res) => {
     const { site, product, timestamp } = req.params;
+    // Construct URL for the radar image based on site, product, and timestamp
     const url = `https://radar.weather.gov/ridge/standard/${site}/${product}_${timestamp}.png`;
 
     try {
+        // Fetch the radar image using axios
         const response = await axios.get(url, { responseType: 'arraybuffer' });
         res.set('Content-Type', 'image/png');
         res.send(response.data);
@@ -21,6 +23,7 @@ app.get('/proxy/radar/:site/:product/:timestamp.png', async (req, res) => {
     }
 });
 
+// Start the server
 app.listen(3000, () => {
     console.log('Proxy server running on http://localhost:3000');
 });
